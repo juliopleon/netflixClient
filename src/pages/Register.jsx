4adaa9b07e5eb5698492c854ce998b770c4024ab
Julio Leon <juliopena143@icloud.com>
2023-04-28 19:52:37 -0600

    details page and register page

---
+++ src/pages/Register.jsx
@@ -0,0 +1,23 @@
+import "./register.scss"
+
+export default function Register() {
+    return (
+        <div className="register">
+            <div className="top">
+                <img className="logo"
+                    src=""
+                    alt=""
+                />
+                <button className="loginButton">Sign In</button>
+            </div>
+            <div className="container">
+                <h1>Unlimited movies and Tv Shows, and more</h1>
+                <h2>Watch anywhere, cancel anytime.</h2>
+                <p>
+                    Ready to watch? Enter your email to sign up or Sign in
+                </p>
+
+            </div>
+        </div>
+    )
+}
