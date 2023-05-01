be8fa12bb365ec12130b04db107d2d9efb7cc74e
Julio Leon <juliopena143@icloud.com>
2023-04-30 19:31:00 -0600

    login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./app.scss"
-import { Home } from "./pages/Home";
-// import Login from "./pages/Login";
-import Register from "./pages/Register";
+// import { Home } from "./pages/Home";
+import Login from "./pages/Login";
+// import Register from "./pages/Register";
 // import Watch from "./pages/Watch";
 
 const App = () => {
-    return <Home />;
+    return <Login />;
 };
 
 export default App;
