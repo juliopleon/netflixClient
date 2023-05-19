7534beecfa1795039e8f93a4f7da9f4b4df2b8ca
Julio Leon <juliopena143@icloud.com>
2023-05-19 15:58:52 -0600

    backend paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import "./app.scss";
-// import { Home } from "./pages/Home";
+import { Home } from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 // import Watch from "./pages/Watch";
 
+import {
+    BrowserRouter,
+    // Router,
+    Routes,
+    Route,
+    Link,
+} from "react-router-dom";
+
 const App = () => {
-    return <Login />;
+    return (<BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/movies" element={<Home type="movies" />} />
+            <Route path="/movies" element={<Home type="movies" />} />
+        </Routes>
+    </BrowserRouter>
+    );
 };
 
 export default App;
